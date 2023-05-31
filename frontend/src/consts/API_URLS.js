e5225ea287b842fc530f3b1d7e8231e60adde600
Alenaglmv <galimova.2002@mail.ru>
2023-06-01 02:51:29 +0500

    Объявила константы для запросов

---
+++ frontend/src/consts/API_URLS.js
@@ -0,0 +1,7 @@
+const BASE_API_URL_ROUTE = `${process.env.REACT_APP_PROJECT_API}/route`;
+const BASE_API_URL_STOP = `${process.env.REACT_APP_PROJECT_API}/stop`;
+
+export {
+    BASE_API_URL_ROUTE,
+    BASE_API_URL_STOP
+}
---
+++ frontend/src/consts/HTTP.js
@@ -0,0 +1,12 @@
+export const HTTP_METHODS = {
+    GET: 'GET',
+    POST: 'POST',
+    PUT: 'PUT',
+    DELETE: 'DELETE',
+    PATCH: 'PATCH'
+};
+
+export const REQUEST_TYPE = {
+    STOP: 'stop',
+    ROUTE: 'route'
+};
