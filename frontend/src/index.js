0e90012f6bc26fc069aff6f5381dffe29b477519
Alenaglmv <galimova.2002@mail.ru>
2023-06-02 11:50:27 +0500

    дописала хранилище данных в подключение к api

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,19 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { ConfigProvider } from 'antd';
+import { Provider } from "react-redux";
+import App from './App';
+import { store } from './redux/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-    <BrowserRouter>
-        <ConfigProvider
-            theme={{
-                token: {
-                    colorPrimary: '#1373E5',
-                    fontFamily: 'Nunito'
-                }
-            }}
-        >
-        </ConfigProvider>
-    </BrowserRouter>
+    <Provider store={store}>
+        <BrowserRouter>
+            <ConfigProvider
+                theme={{
+                    token: {
+                        colorPrimary: '#1373E5',
+                        fontFamily: 'Nunito'
+                    }
+                }}
+            >
+                <App />
+            </ConfigProvider>
+        </BrowserRouter>
+    </Provider>
 );
